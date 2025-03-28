7840af327a3c5fa4f7e91f0a44b7fb0ab50b8d92
MatheusDamascenoVaz <matheusdamascenovaz@gmail.com>
2025-03-28 19:05:40 -0300

    Atualização da tela menu principal

--- ProjetoIntegrador/ProjetoIntegrador/Screen/MenuPrincipal.cs
+++ ProjetoIntegrador/ProjetoIntegrador/Screen/MenuPrincipal.cs
@@ -16,5 +16,10 @@ namespace ProjetoIntegrador.Screen
         {
             InitializeComponent();
         }
+
+        private void MenuPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
