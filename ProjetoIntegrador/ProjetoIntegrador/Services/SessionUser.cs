a2ef61d6bd3b2b5615f92c36bbc174927291f6a1
MatheusDamascenoVaz <matheusdamascenovaz@gmail.com>
2025-04-08 19:45:04 -0300

    Resolução de conflito com merge q havia ficado pendente

--- ProjetoIntegrador/ProjetoIntegrador/Services/SessionUser.cs
+++ ProjetoIntegrador/ProjetoIntegrador/Services/SessionUser.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConexaoMySQL.Services
+namespace ProjetoIntegrador.Services
 {
     public static class SessionUser
     {
