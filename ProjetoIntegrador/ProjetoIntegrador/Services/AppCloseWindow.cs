e9c3f345c420f2366a30bd3b6a5a4ad2eee4df59
MatheusDamascenoVaz <matheusdamascenovaz@gmail.com>
2025-04-09 20:27:41 -0300

    Inclusão da branch para a criação de métodos MVC do fechamento de janelas

---
+++ ProjetoIntegrador/ProjetoIntegrador/Services/AppCloseWindow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoIntegrador.Services
+{
+    public static class AppCloseWindow
+    {
+        public static void CloseApp(object sender, FormClosingEventArgs e) {
+
+              Application.ExitThread();
+        
+        }
+    }
+}
