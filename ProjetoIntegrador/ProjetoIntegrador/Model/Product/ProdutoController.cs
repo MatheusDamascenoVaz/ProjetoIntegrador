e9da5f2f08e1bf217121a0a25309664e2540a25f
clemente-jey <jonasclemente.contato@gmail.com>
2025-04-11 19:07:35 -0300

    Correções na classe de cadastrar produtos.

---
+++ ProjetoIntegrador/ProjetoIntegrador/Model/Product/ProdutoController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjetoIntegrador.Services;
+
+namespace ProjetoIntegrador.Model.Product
+{
+    class ProdutoController
+    {
+
+        private readonly ProductRepositorio _productRepositorio;
+
+        public ProdutoController(ProductRepositorio productRepositorio)
+        {
+            _productRepositorio = productRepositorio;
+        }
+
+
+        public bool RegisterProduct(Produto produto)
+        {
+            try
+            {
+                return _productRepositorio.Register(produto);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erro ao registrar produto: " + ex.Message);
+            }
+        }
+
+
+
+    }
+}
