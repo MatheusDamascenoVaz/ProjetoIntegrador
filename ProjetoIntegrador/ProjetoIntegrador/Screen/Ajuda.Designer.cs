dbc2518add6cbce990cd64aedaba1c5bde6391ec
thaia00 <brasilbarretothaiara@gmail.com>
2025-04-30 20:15:04 -0300

    Alteração tela de ajuda

--- ProjetoIntegrador/ProjetoIntegrador/Screen/Ajuda.Designer.cs
+++ ProjetoIntegrador/ProjetoIntegrador/Screen/Ajuda.Designer.cs
@@ -29,21 +29,241 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Ajuda));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.PowderBlue;
+            this.button1.Location = new System.Drawing.Point(-1, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(801, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "LOGIN";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.PowderBlue;
+            this.button2.Location = new System.Drawing.Point(-1, 147);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(801, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "MENU PRINCIPAL";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.PowderBlue;
+            this.button3.Location = new System.Drawing.Point(-1, 201);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(801, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "ADCIONAR USUÁRIO";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.PowderBlue;
+            this.button4.Location = new System.Drawing.Point(-1, 246);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(801, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "ADCIONAR PRODUTOS";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.PowderBlue;
+            this.button5.Location = new System.Drawing.Point(-1, 302);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(801, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "ADCIONAR VENDAS";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.PowderBlue;
+            this.button6.Location = new System.Drawing.Point(-1, 357);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(801, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "GERAR RELATÓRIO";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(157, 118);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(466, 16);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "nsira usuário e senha para acessar. Opção para recuperar senha esquecida.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(-4, 173);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(908, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Acesso a todas as outras páginas: adicionar usuário (restrito a administradores)," +
+    " adicionar vendas, adicionar produtos, gerar relatórios e ajuda. Permite voltar " +
+    "ao login";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(70, 227);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(661, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Cadastro de novos usuários (email, telefone, nome, senha, matrícula). Acesso excl" +
+    "usivo para administradores.\n";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(93, 272);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(638, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Gerencie o catálogo: adicione, altere e remova produtos. Busque produtos por nome" +
+    " ou código de barras.";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(145, 328);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(519, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Registre vendas inserindo código de barras, quantidade e adicionando itens à vend" +
+    "a.";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(269, 383);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(289, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Escolha o tipo de relatório desejado e imprima.";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(199, 23);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(376, 24);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Manual de Uso do Gerenciador de Estoque";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(175, 47);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(437, 16);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Este sistema possui sete páginas principais para gerenciar seu estoque";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(25, 428);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(754, 13);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Este manual visa auxiliar na utilização eficiente de todas as funcionalidades do " +
+    "sistema. Em caso de dúvidas, consulte este guia ou o administrador do sistema.";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::ProjetoIntegrador.Properties.Resources.Captura_de_tela_2025_04_28_203346;
+            this.pictureBox1.Location = new System.Drawing.Point(651, 8);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(137, 78);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
             // 
             // Ajuda
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Ajuda";
             this.Text = "Ajuda";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
