84d6c6e1f8b1309e08b9ea334b8f34772c6aa393
clemente-jey <jonasclemente.contato@gmail.com>
2025-05-09 21:24:40 -0300

    alteração  no designer da tela de relatório

--- ProjetoIntegrador/ProjetoIntegrador/Screen/AddRelatorio.Designer.cs
+++ ProjetoIntegrador/ProjetoIntegrador/Screen/AddRelatorio.Designer.cs
@@ -30,7 +30,6 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddRelatorio));
             this.label1 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.dTimeInicio = new System.Windows.Forms.DateTimePicker();
             this.button1 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
@@ -41,57 +40,44 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
-            this.comboRelatorios = new System.Windows.Forms.ComboBox();
-            this.btnGerarRelatorio = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(123, 61);
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(196, 53);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(49, 13);
+            this.label1.Size = new System.Drawing.Size(84, 24);
             this.label1.TabIndex = 0;
             this.label1.Text = "Relatório";
             // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
-            "Relatório de Produtos em Estoque",
-            "Relatório de Produtos Vencidos",
-            "Relatório de Produtos Próximos a Data de Validade",
-            "Relatório de Usuários"});
-            this.comboBox1.Location = new System.Drawing.Point(126, 77);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 2;
-            this.comboBox1.Text = "Tipo de Relatório";
-            // 
             // dTimeInicio
             // 
-            this.dTimeInicio.Location = new System.Drawing.Point(126, 143);
+            this.dTimeInicio.Cursor = System.Windows.Forms.Cursors.AppStarting;
+            this.dTimeInicio.Location = new System.Drawing.Point(147, 272);
             this.dTimeInicio.Name = "dTimeInicio";
-            this.dTimeInicio.Size = new System.Drawing.Size(84, 20);
+            this.dTimeInicio.Size = new System.Drawing.Size(230, 20);
             this.dTimeInicio.TabIndex = 3;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.button1.Cursor = System.Windows.Forms.Cursors.AppStarting;
-            this.button1.Location = new System.Drawing.Point(86, 282);
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.Location = new System.Drawing.Point(147, 196);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(161, 23);
+            this.button1.Size = new System.Drawing.Size(262, 34);
             this.button1.TabIndex = 4;
-            this.button1.Text = "Gerar Relatório Produtos ";
+            this.button1.Text = "Gerar Relatório Produtos próximos ao vencimento";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(123, 117);
+            this.label2.Location = new System.Drawing.Point(144, 246);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(33, 13);
             this.label2.TabIndex = 5;
@@ -100,7 +86,7 @@
             // lblDataInicio
             // 
             this.lblDataInicio.AutoSize = true;
-            this.lblDataInicio.Location = new System.Drawing.Point(96, 150);
+            this.lblDataInicio.Location = new System.Drawing.Point(117, 279);
             this.lblDataInicio.Name = "lblDataInicio";
             this.lblDataInicio.Size = new System.Drawing.Size(24, 13);
             this.lblDataInicio.TabIndex = 6;
@@ -109,7 +95,7 @@
             // lblDataFim
             // 
             this.lblDataFim.AutoSize = true;
-            this.lblDataFim.Location = new System.Drawing.Point(94, 187);
+            this.lblDataFim.Location = new System.Drawing.Point(115, 312);
             this.lblDataFim.Name = "lblDataFim";
             this.lblDataFim.Size = new System.Drawing.Size(26, 13);
             this.lblDataFim.TabIndex = 7;
@@ -117,16 +103,16 @@
             // 
             // dTimeFinal
             // 
-            this.dTimeFinal.Location = new System.Drawing.Point(126, 181);
+            this.dTimeFinal.Location = new System.Drawing.Point(147, 305);
             this.dTimeFinal.Name = "dTimeFinal";
-            this.dTimeFinal.Size = new System.Drawing.Size(84, 20);
+            this.dTimeFinal.Size = new System.Drawing.Size(230, 20);
             this.dTimeFinal.TabIndex = 8;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(276, 282);
+            this.button2.Location = new System.Drawing.Point(147, 126);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(242, 23);
+            this.button2.Size = new System.Drawing.Size(209, 23);
             this.button2.TabIndex = 10;
             this.button2.Text = "Gerar Relatório Produtos Vencidos";
             this.button2.UseVisualStyleBackColor = true;
@@ -134,7 +120,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(68, 338);
+            this.button3.Location = new System.Drawing.Point(147, 155);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(209, 23);
             this.button3.TabIndex = 11;
@@ -144,9 +130,9 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(321, 336);
+            this.button4.Location = new System.Drawing.Point(147, 97);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(216, 23);
+            this.button4.Size = new System.Drawing.Size(209, 23);
             this.button4.TabIndex = 12;
             this.button4.Text = "Gerar Relatório de Produtos em Estoque";
             this.button4.UseVisualStyleBackColor = true;
@@ -154,39 +140,25 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(68, 385);
+            this.button5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.button5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button5.Location = new System.Drawing.Point(147, 354);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(309, 36);
+            this.button5.Size = new System.Drawing.Size(262, 36);
             this.button5.TabIndex = 13;
             this.button5.Text = "Gerar Relatório em um intervalo de tempo";
-            this.button5.UseVisualStyleBackColor = true;
+            this.button5.UseVisualStyleBackColor = false;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
-            // comboRelatorios
+            // pictureBox1
             // 
-            this.comboRelatorios.FormattingEnabled = true;
-            this.comboRelatorios.Location = new System.Drawing.Point(321, 77);
-            this.comboRelatorios.Name = "comboRelatorios";
-            this.comboRelatorios.Size = new System.Drawing.Size(121, 21);
-            this.comboRelatorios.TabIndex = 14;
-            // 
-            // btnGerarRelatorio
-            // 
-            this.btnGerarRelatorio.Location = new System.Drawing.Point(524, 140);
-            this.btnGerarRelatorio.Name = "btnGerarRelatorio";
-            this.btnGerarRelatorio.Size = new System.Drawing.Size(202, 61);
-            this.btnGerarRelatorio.TabIndex = 15;
-            this.btnGerarRelatorio.Text = "GERAR RELATÓRIO";
-            this.btnGerarRelatorio.UseVisualStyleBackColor = true;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(355, 164);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(35, 13);
-            this.label3.TabIndex = 16;
-            this.label3.Text = "label3";
+            this.pictureBox1.Image = global::ProjetoIntegrador.Properties.Resources.EstoqueMasterLogo;
+            this.pictureBox1.Location = new System.Drawing.Point(683, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(66, 51);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
             // 
             // AddRelatorio
             // 
@@ -194,9 +166,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightBlue;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.btnGerarRelatorio);
-            this.Controls.Add(this.comboRelatorios);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
@@ -207,12 +177,12 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dTimeInicio);
-            this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "AddRelatorio";
             this.Text = "Extrair Relatório";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -221,7 +191,6 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.ComboBox comboBox1;
         private System.Windows.Forms.DateTimePicker dTimeInicio;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label2;
@@ -232,8 +201,6 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.ComboBox comboRelatorios;
-        private System.Windows.Forms.Button btnGerarRelatorio;
-        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
